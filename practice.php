71f4bb51424dc56a686c5f225a557e0b79a879d9
nemoto <samuko.atuatu@gmail.com>
2020-04-04 14:25:23 +0000

    2nd commit

--- practice.php
+++ practice.php
@@ -1,21 +1,22 @@
 <?php
 //1
 function a($x){
-    echo $x*2;
+    $y= $x*2;
+    return $y;
 }
 
-//a(3);
+//echo a(3);
 
 //2
 function sum($a,$b){
-    echo $a + $b;
+    $ab = $a + $b;
+    return $ab;
 }
 
-//sum(1,2);
+//echo sum(1,2);
 
 //3
 $arr1 = array(1,3,5,7,9);
-$total = 1;
 function i($arr){
     $total = 1;
     foreach ($arr as $value){
@@ -39,9 +40,28 @@ function max_array($arr){
 
 //echo max_array(array(1,2,3,4,5));
 
-strip_tags
-array_push
-array_merge
-time, mktime
-date
+//5.
+//strip_tags
+$i = '<p>abc</p>';
+//echo strip_tags($i);
+//echo $i;
+
+//array_push
+$a = [1,2,3];
+//array_push($a,4,5);
+//print_r($a);
+
+//array_merge
+$b = ["赤" => "りんご","黄" => "バナナ",1,2];
+$c = ["赤" => "薔薇","ピンク" => "桜",3,4];
+$bc = array_merge($b,$c);
+//print_r($bc);
+
+//time, mktime
+//echo "現在のタイムスタンプは".time()."です。";
+//echo mktime(date('H'),date('i'),date('s'),date('m'),date('d'),date('Y'));
+
+//date
+//echo date('Y/m/d');
+
 ?>
